0c7ad4237033eae63b0408ede6e3bde19c9f5278
dujesa <dujesaric996@gmail.com>
2023-06-09 23:26:25 +0200

    Implemented splash screen and menu screen

---
+++ frontend/src/hooks/useTimeout/index.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useRef } from "react";
+
+export const useTimeout = (callback: () => void, delay: number) => {
+  const timeoutRef = useRef(0);
+  const savedCallback = useRef(callback);
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    const tick = () => savedCallback.current();
+
+    if (typeof delay === "number") {
+      timeoutRef.current = window.setTimeout(tick, delay);
+
+      return () => window.clearTimeout(timeoutRef.current);
+    }
+  }, [delay]);
+
+  return timeoutRef;
+};
+
+export default useTimeout;
