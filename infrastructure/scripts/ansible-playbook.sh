00e92d84d196096d0da6ff66c83c8bd0f6ca1d3b
Bartol Deak <b@bdeak.net>
2023-07-26 02:58:55 +0200

    api ansible roles

--- infrastructure/scripts/ansible-playbook.sh
+++ infrastructure/scripts/ansible-playbook.sh
@@ -23,7 +23,11 @@ fi
 
 ./ssh-agent.sh "$ANSIBLE_ENV" load
 
-ansible-playbook "../ansible/playbooks/$ANSIBLE_COMPONENT/playbook.yml" \
-  -i "../ansible/inventories/$ANSIBLE_ENV.aws_ec2.yml" "$@"
+cd ../ansible || exit 1
+
+ansible-playbook "playbooks/$ANSIBLE_COMPONENT/playbook.yml" \
+  -i "inventories/$ANSIBLE_ENV.aws_ec2.yml" "$@"
+
+cd - || exit 1
 
 ./ssh-agent.sh "$ANSIBLE_ENV" unload
