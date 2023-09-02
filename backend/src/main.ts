e4f649811828149afb4c7369fbbfb6675cf40af9
Bartol Deak <b@bdeak.net>
2023-09-02 16:50:23 +0200

    add cors origins

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,11 +6,15 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-    credentials: true,
+    origin: [
+      'capacitor://localhost',
+      'ionic://localhost',
+      'http://localhost',
+      'http://localhost:5173',
+      'http://localhost:3000',
+      'https://safetynet-web-production.dump.hr',
+      'https://safetynet-kviz.skole.hr',
+    ],
   });
 
   const prismaService = app.get(PrismaService);
