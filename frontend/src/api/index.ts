ee26c42797b8fcf9d214a7f36a5733bd10e9b80b
Bartol Deak <b@bdeak.net>
2023-09-02 17:37:50 +0200

    try to fix cors errors again

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,5 +1,23 @@
 export * from './query';
 
+import axios, { AxiosError, AxiosResponse } from 'axios';
+
+export const api = axios.create({
+  baseURL: import.meta.env.VITE_APP_API_ENDPOINT,
+  timeout: 5000,
+});
+
+type ErrorResponse = AxiosError & {
+  response: AxiosResponse<{ message: string }>;
+};
+
+api.interceptors.response.use(
+  (response) => response.data,
+
+  (error: ErrorResponse) =>
+    Promise.reject(error.response.data.message || error.message)
+);
+
 export const fetchAPI = (url: string, params?: RequestInit) =>
   fetch(
     `${url.startsWith('/') ? import.meta.env.VITE_APP_API_ENDPOINT : ''}${url}`,
