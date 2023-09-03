c077940ce1b7ae718c476044a586e38bfe115ed6
Bartol Deak <b@bdeak.net>
2023-09-03 14:43:37 +0200

    remove cors settings from nest

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,20 +5,6 @@ import { PrismaService } from './prisma.service';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: [
-      'capacitor://localhost',
-      'ionic://localhost',
-      'http://localhost',
-      'http://localhost:5173',
-      'http://localhost:3000',
-      'https://safetynet-web-production.dump.hr',
-      'https://safetynet-kviz.skole.hr',
-    ],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  });
-
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
 
