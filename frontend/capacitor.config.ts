0549fa8a6c3a1d523d4c900f4477f2dcbafb5ee4
Lucija Fradelić <52828220+lucijs@users.noreply.github.com>
2025-11-28 22:46:13 +0100

    Updated android app version

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'hr.safetynet.app',
+  appId: 'hr.safetynet2025.app',
   appName: 'SafetyNet',
   webDir: 'dist',
   server: {
